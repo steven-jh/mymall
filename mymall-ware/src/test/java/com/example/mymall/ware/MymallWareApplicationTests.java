a4210a4a01712a21d62bb4dd9bd8b7f618af16ff
steven-jh <1610854737@qq.com>
2020-07-15 21:41:55 +0800

    项目初始结构创建

---
+++ mymall-ware/src/test/java/com/example/mymall/ware/MymallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mymall.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymallWareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
